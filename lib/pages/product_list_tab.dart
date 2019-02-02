b62edc6619743bae6f7fdaec02432a1af8e3fece
San Neupane <123here.comes.san@gmail.com>
2019-02-02 21:20:41 +0545

    added dialogbox for failed delete operation.

--- lib/pages/product_list_tab.dart
+++ lib/pages/product_list_tab.dart
@@ -3,31 +3,28 @@ import 'package:practise_app1/scoped-models/main.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class ListProduct extends StatefulWidget {
-
   final MainModel model;
   ListProduct(this.model);
 
   @override
-  State<StatefulWidget> createState() {    
+  State<StatefulWidget> createState() {
     return _ListProduct();
   }
-
 }
 
-class _ListProduct extends State<ListProduct>{
-
+class _ListProduct extends State<ListProduct> {
   @override
-  initState(){
+  initState() {
     widget.model.fetchProducts();
     super.initState();
   }
+
   Widget _buildEditButton(BuildContext context, int index, MainModel model) {
     return IconButton(
       icon: Icon(Icons.edit),
       onPressed: () {
         model.selectProduct(model.allproducts[index].id);
-        Navigator.pushNamed(context, '/editProduct')
-        .then((_){
+        Navigator.pushNamed(context, '/editProduct').then((_) {
           model.selectProduct(null);
         });
       },
@@ -35,20 +32,34 @@ class _ListProduct extends State<ListProduct>{
   }
 
   Widget _build(BuildContext context, int index, MainModel model) {
-    Color colorv = Colors.red;
     return Dismissible(
       key: Key(model.allproducts[index].title),
       onDismissed: (DismissDirection direction) {
-        if (direction == DismissDirection.startToEnd) {
-          model.selectProduct(model.allproducts[index].id);
-          model.deleteProduct();
-        }else{
-          model.selectProduct(model.allproducts[index].id);
-          model.deleteProduct();
-        }
+        model.selectProduct(model.allproducts[index].id);
+        model.deleteProduct().then((bool isSuccess) {
+          if (!isSuccess) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Something Went Wrong !!!"),
+                  content: Text("Try deleting products later"),
+                  actions: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Okay !"),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        });
       },
       background: Container(
-        color: colorv,
+        color: Colors.red,
       ),
       child: Column(
         children: <Widget>[
